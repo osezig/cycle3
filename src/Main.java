35141cfdfd55660d2223ec5c4a04799a9ba1e2da
sergey_iagfarov <dedaz27@mail.ru>
2022-10-06 20:29:59 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         int salary = 29000;
         int total = 0;
-        for (int i = 0; i < 12; i++) {
+        for (int i = 1; i <= 12; i++) {
             total = total + total/100;
             total = total + salary;
 
-            System.out.println("Месяц, сумма накопление " + total);
+            System.out.println("Месяц " + i + ", сумма накопление " + total);
         }
     }
 }
